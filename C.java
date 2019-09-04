65ec4b5e22678d251455d866c3dcfea1ed20d3e7
PamidimarriRushitha <52276540+PamidimarriRushitha@users.noreply.github.com>
2019-09-04 16:59:25 +0530

    Update C.java

--- C.java
+++ C.java
@@ -4,9 +4,15 @@ public class C
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
-    int N=sc.nextInt();
-    int K=sc.nextInt();
+    int a[]=new int[2];
+      for(int i=0; i<a.length; i++)
+    {
+      a[i]=sc.nextInt();
+    }
+    int N=a[0];
+    int K=a[1];
     int count=0;
+    
     int arr[]=new int[N];
     for(int i=0; i<arr.length; i++)
     {
