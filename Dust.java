7457206833b2fe31a3a316e0a7aea9b151d17aae
PamidimarriRushitha <52276540+PamidimarriRushitha@users.noreply.github.com>
2019-09-04 18:15:17 +0530

    Update and rename C.java to Dust.java

--- Dust.java
+++ Dust.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
-public class C
+public class Dust
 {
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
-    int a[]=new int[2];
-      for(int i=0; i<a.length; i++)
-    {
-      a[i]=sc.nextInt();
-    }
-    int N=a[0];
-    int K=a[1];
+    int N=sc.nextInt();
+    int  K=sc.nextInt();
     int count=0;
     
     int arr[]=new int[N];
-    for(int i=0; i<arr.length; i++)
+    for(int i=0;i<N;i++)    
     {
       arr[i]=sc.nextInt();
     }
-    for(int i=0;i<arr.length;i++)
+    for(int i=0;i<N;i++)  
     {
       if(arr[i]==K)
       {
