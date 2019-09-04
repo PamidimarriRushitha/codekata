93d4784dc2a41989010b93b5ef51b65b0414a17b
PamidimarriRushitha <52276540+PamidimarriRushitha@users.noreply.github.com>
2019-09-04 16:03:32 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class A {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int s=0;
+      if(a>0)
+      {
+        s=a*(a+1)/2;
+    }
+      System.out.println(s);
+}
+}
