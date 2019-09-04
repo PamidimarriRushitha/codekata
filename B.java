e13553feb96ab3f31a467bb6dc095697e5f2b161
PamidimarriRushitha <52276540+PamidimarriRushitha@users.noreply.github.com>
2019-09-04 16:29:22 +0530

    Create B.java

---
+++ B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class B {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int N=sc.nextInt();
+      if(1<=N && N<=10000)
+      {
+System.out.println(~N);
+      }
+      
+    }
+}
